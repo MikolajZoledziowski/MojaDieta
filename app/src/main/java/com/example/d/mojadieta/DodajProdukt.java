1268ce222deb52079c45b9005e6cf018820304b5
Mikołaj Żołędziowski <frost1111@wp.pl>
2017-05-22 21:02:53 +0200

    Widok dodaj produkt
    Przejścia między widokami. Pusty widok

--- app/src/main/java/com/example/d/mojadieta/DodajProdukt.java
+++ app/src/main/java/com/example/d/mojadieta/DodajProdukt.java
@@ -3,19 +3,13 @@ package com.example.d.mojadieta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Produkt extends AppCompatActivity {
+public class DodajProdukt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_produkt);
+        setContentView(R.layout.activity_dodaj_produkt);
         Intent intent = getIntent();
-    }
-    public void ProduktDodaj(View view) {
-        Intent intent = new Intent(this, DodajProdukt.class);
-
-        startActivity(intent);
     }
 }
