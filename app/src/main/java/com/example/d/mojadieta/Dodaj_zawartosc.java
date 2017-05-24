2ced65cbc2be8818318a28cb9306e924bb20cba5
Mikołaj Żołędziowski <frost1111@wp.pl>
2017-05-24 16:24:39 +0200

    Poprawki

--- app/src/main/java/com/example/d/mojadieta/Dodaj_zawartosc.java
+++ app/src/main/java/com/example/d/mojadieta/Dodaj_zawartosc.java
@@ -3,13 +3,20 @@ package com.example.d.mojadieta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Dodaj_zawartosc extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_zawartosc);
+        spinner = (Spinner) findViewById(R.id.spinner3);
+
+        ArrayAdapter adapter =ArrayAdapter.createFromResource(this,R.array.produkty_array,android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
         Intent intent = getIntent();
     }
 }
