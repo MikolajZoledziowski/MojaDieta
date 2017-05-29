efe78715a3b2bbbf5f9304d8ae4fc6729ee8ee73
Mikołaj Żołędziowski <frost1111@wp.pl>
2017-05-29 21:24:12 +0200

    ehh

--- app/src/main/java/com/example/d/mojadieta/DodajProdukt.java
+++ app/src/main/java/com/example/d/mojadieta/DodajProdukt.java
@@ -4,21 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class DodajProdukt extends AppCompatActivity {
     Spinner spinner;
-    @Override
-
+     EditText editText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_produkt);
         Intent intent = getIntent();
+        //lista
         spinner=(Spinner) findViewById(R.id.spinner2);
         ArrayAdapter adapter =ArrayAdapter.createFromResource(this,R.array.Rodzajewag,android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+        //czyszczenie
+        editText = (EditText)findViewById(R.id.editText);
+        editText.setOnClickListener(this);
     }
 
+
+    public void Wyczysc() {
+        editText.setText("");
+    }
 
 
 }
